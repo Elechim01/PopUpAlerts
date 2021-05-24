406ddc478872b0939c8ae0c7769d5f39be168512
Elechim01 <mikymanny015@gmail.com>
2021-05-24 19:15:19 +0200

    Initial Commit

---
+++ PopUpAlerts/PopUpAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpAlertsApp.swift
+//  PopUpAlerts
+//
+//  Created by Michele Manniello on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PopUpAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
